5d7a1d25db74a04d937e95ccc17bf8c0441d3b81
Co Chi Tam <cochitam1990@gmail.com>
2017-01-17 16:04:05 +0700

    + Add script.

---
+++ Multiplayer_RPG/Assets/Scripts/UI/CClickable.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+namespace UICustomize {
+	[RequireComponent (typeof(MaskableGraphic))]
+	public class CClickable : MonoBehaviour, IPointerUpHandler, IPointerDownHandler, 
+									IPointerEnterHandler, IPointerExitHandler, 
+									IPointerClickHandler
+	{
+
+		#region Properties
+
+		public UnityEvent OnEventPointerUp;
+		public UnityEvent OnEventPointerDown;
+		public UnityEvent OnEventPointerEnter;
+		public UnityEvent OnEventPointerExit;
+		public UnityEvent OnEventPointerClick;
+
+		#endregion
+
+		#region Interface implementation
+
+		public void OnPointerUp (PointerEventData eventData)
+		{
+			OnEventPointerUp.Invoke ();
+		}
+
+		public void OnPointerDown (PointerEventData eventData)
+		{
+			OnEventPointerDown.Invoke ();
+		}
+
+		public void OnPointerEnter (PointerEventData eventData)
+		{
+			OnEventPointerEnter.Invoke ();
+		}
+
+		public void OnPointerExit (PointerEventData eventData)
+		{
+			OnEventPointerExit.Invoke ();
+		}
+
+		public void OnPointerClick (PointerEventData eventData)
+		{
+			OnEventPointerClick.Invoke ();
+		}
+
+		#endregion
+	}
+}
