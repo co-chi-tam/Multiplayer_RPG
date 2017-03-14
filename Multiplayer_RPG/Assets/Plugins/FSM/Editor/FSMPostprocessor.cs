b67bb58f4ded4a7c1d5d82c0981b6aa0dab8d084
Co Chi Tam <cochitam1990@gmail.com>
2017-03-14 16:50:42 +0700

    Add Postprocessor

---
+++ Multiplayer_RPG/Assets/Plugins/FSM/Editor/FSMPostprocessor.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.IO;
+using System.Xml;
+
+public class FSMPostprocessor: AssetPostprocessor {
+
+	static void OnPostprocessAllAssets (string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths) 
+	{
+		foreach (string str in importedAssets)
+		{
+			var path = Environment.CurrentDirectory + "/" + str;
+			var extension = Path.GetExtension (path);
+			if (extension == ".graphml") {
+				ReadNode (path.Replace("/", "\\").Replace ("\\", "\\"));
+			}
+		}
+	}
+
+	static void ReadNode(string path) {
+		var stReader = new StreamReader (path);
+		XmlReader xReader = XmlReader.Create (new StringReader (stReader.ReadToEnd()));
+		while (xReader.Read ()) {
+			if (xReader.IsStartElement()) {
+				switch (xReader.Name) {
+				case "node":
+					var nodeID = xReader ["id"].ToString ();
+					Debug.Log (nodeID);
+					break;
+				case "y:ShapeNode":
+
+					break;
+				case "y:NodeLabel":
+					if (xReader.Read()) {
+						var nodeLabel = xReader.Value.Trim ();
+						Debug.Log (nodeLabel);
+					}
+					break;
+				case "edge":
+					var source = xReader ["source"].ToString ();
+					var target = xReader ["target"].ToString ();
+					Debug.Log (source + " -> " + target);
+					break;
+				case "y:EdgeLabel":
+					if (xReader.Read()) {
+						var edgeLabel = xReader.Value.Trim ();
+						Debug.Log (edgeLabel);
+					}
+					break;
+				}
+			}
+		}
+	}
+
+}
